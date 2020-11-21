d77c4e73b24cf3e6b1d010ba4d55edc39f6da6c1
pfullen <pfullen.code@gmail.com>
2020-11-21 17:43:19 -0500

    fix server

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
-var LoremIpsum = require('lorem-ipsum').LoremIpsum;
 
-var lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
 
-app.get('/', (req, res) => res.send(lorem.generateParagraphs(7)))
+app.get('/', (req, res) => res.send('Hello World'))
+
+
+app.get('/getClients', function (req, res) {
+  res.json({"Test": "Hellow world"})
+ });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
